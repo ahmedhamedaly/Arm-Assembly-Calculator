efee7b7e6b123536ccdede63c3f8befe2a8f1236
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-17 15:15:46 +0000

    added a lot of shit

--- Assignment2/calculator.s
+++ Assignment2/calculator.s
@@ -12,6 +12,18 @@ IO1SET	EQU	0xE0028014
 IO1CLR	EQU	0xE002801C
 IO1PIN  EQU 0xE0028010
 
+; main
+; @PARAMETERS: args[]
+; @RETURNS: 0
+;
+  BL initSerial
+
+while
+  BL getKey
+  BL handleKey
+  BL displayN
+  B  while
+
 ; getKey
 ; @PARAMETERS: None
 ; @RETURNS: R0 -> buttonIndex
@@ -20,22 +32,37 @@ getKey
   PUSH {R1-R12, LR}
 
   MOV R0, #0  ; buttonIndex = 0
-  MOV R1, #1  ; listening = True
-  LDR R2, =IO1PIN
-  LDR R3, =0x00F00000
+  LDR R1, =4000000 ; delay = 4000000
+  LDR R2, =IO1PIN ; init[io1pins]
+  LDR R3, =0x00F00000 ; maskPins = 0x00F00000
 
+gkWhile           ; do {
+  LDR R4, [R2]    ; load[pins]
+  AND R4, R4, R3  ; pins = pins & mask
+  CMP R4, R3      ; }
+  BEQ gkWhile     ; while (pins == mask)
 
-gkWhile
-  CMP R1, #1  ; while(listening)
-  BNE gkEnd   ; {
-  LDR R4, [R2]  ; load[pins]
-  AND R4, R4, R3  ;
-  CMP R4, R3
-  BEQ gkWhile
-  MOV R1, #0  ; listening = False
-  B   gkWhile ; }
+  MOV R5, R4      ; pressedButton = pins
+gkLongW
+  CMP R5, R4      ; while (pressedButton == pins)
+  BNE gkPress     ; {
+  CMP R6, R1      ; if (delayCounter != delay)
+  BEQ gkLong      ; {
+  LDR R4, [R2]    ; load[pins]
+  AND R4, R4, R3  ; pins = pins & mask
+  ADD R6, R6, #1  ; delayCounter++
+  B   gkLongW     ; }
 
-  gkEnd
+gkLong
+  MOV R0, R6      ; buttonIndex = pins
+  BL  index
+  RSB R0, R0, #0  ; NEG(buttonIndex)
+  B   gkEnd
+gkPress
+  MOV R0, R6      ; buttonIndex = pins
+  BL  index
+
+gkEnd
   POP {R1-R12, PC}
 ; handleKey
 ; @PARAMETERS: R0 -> buttonIndex, R1 -> n, R2 -> prevN, R3 -> prevO
@@ -82,41 +109,62 @@ hkEnd
   POP {R4-R12, PC}
 
 ; nAdd
-; @PARAMETERS: R1 -> n, R2 -> prevN, R3 -> prevO
+; @PARAMETERS: R1 -> n
 ; @RETURNS:
 ;
 nAdd
-  PUSH {R0, R4-R12, LR}
+  PUSH {R0, R2-R12, LR}
 
-  ADD R1, R1, #1
-  MOV R2, #1
-  MOV R3, #1
+  ADD R1, R1, #1  ; n++
 
-  POP {R0, R4-R12, LR}
+  POP {R0, R2-R12, LR}
 ; nSub
-; @PARAMETERS:
+; @PARAMETERS: R1 -> n
 ; @RETURNS:
 ;
 nSub
+  PUSH {R0, R2-R12, LR}
 
+  SUB R1, R1, #1  ; n--
+
+  POP {R0, R2-R12, PC}
 ; addOp
-; @PARAMETERS:
+; @PARAMETERS: R1 -> n, R2 -> prevN, R3 -> prevO
 ; @RETURNS:
 ;
 addOP
+  PUSH {R0, R4-R12, LR}
 
+  ADD R4, R1, R2  ; temp = n + prevN
+  MOV R3, #1  ; prevO = True
+  MOV R2, #0  ; prevN = n
+  MOV R1, R4  ; n = temp
+
+  POP {R0, R4-R12, PC}
 ; subOp
-; @PARAMETERS:
+; @PARAMETERS: R1 -> n, R2 -> prevN, R3 -> prevO
 ; @RETURNS:
 ;
 subOp
+  PUSH {R0, R4-R12, LR}
 
+  SUB R4, R1, R2  ; temp = n + prevN
+  MOV R3, #1  ; prevO = True
+  MOV R2, R1  ; prevN = n
+  MOV R1, R4  ; n = temp
+
+  POP {R0, R4-R12, PC}
 ; lastClear
-; @PARAMETERS:
+; @PARAMETERS: R2 -> prevN, R3 -> prevO
 ; @RETURNS:
 ;
 lastClear
+  PUSH {R0-R1, R4-R12, LR}
 
+  MOV R2, #0  ; prevN = 0
+  MOV R3, #0  ; prevO = 0
+
+  POP {R0-R1, R4-R12, LR}
 ; fullClear
 ; @PARAMETERS:
 ; @RETURNS:
@@ -128,13 +176,30 @@ fullClear
   MOV R1, #0
   MOV R2, #0
   MOV R3, #0
+  BL  initSerial
 
   POP {R4-R12, PC}
 ; reverseBits
-; @PARAMETERS:
-; @RETURNS:
+; @PARAMETERS: R1 -> n
+; @RETURNS: R
 ;
+reverseBits
+  PUSH {R0, R2-R12, LR}
 
+  MOV R
+
+  POP {R0, R2-R12, PC}
+
+; displayN
+; @PARAMETERS: R1 -> n
+; @RETURNS: None
+;
+displayN
+  PUSH {R0, R2-R12, LR}
+
+
+
+  POP {R0, R2-R12, PC}
 
 ; delay
 ; @PARAMETERS: None
@@ -149,10 +214,40 @@ delay
 
   POP {R0, PC}
 
+; index
+; @PARAMETERS: R0 -> pins
+; @RETURNS: R0 -> buttonIndex
+;
+index
+  PUSH {R1-R12, LR}
+
+  LDR R1, =0xE00000
+  LDR R2, =0xD00000
+  LDR R3, =0xB00000
+  LDR R4, =0x700000
+
+  CMP R0, R1
+  BNE iTwo
+  MOV R0, #20
+  B   iEnd
+iTwo CMP R0, R2
+  BNE iThree
+  MOV R0, #21
+  B   iEnd
+iThree CMP R0, R3
+  BNE iFour
+  MOV R0, #22
+  B   iEnd
+iFour
+  MOV R0, #23
+
+iEnd
+  POP {R1-R12, PC}
 ; initSerial
 ; @PARAMETERS: None
 ; @RETURNS: None
 ;
+initSerial
   PUSH {R0-R12, LR}
 
   LDR	R1, =IO1DIR
